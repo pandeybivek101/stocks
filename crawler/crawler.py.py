cb0f61c8f9b56ca746c0cc21a50edeefbd4b1303
Bivek Pandey <pandeyvivak25@hotmail.com>
2023-09-15 21:43:30 +0300

    crawler for broker information added

--- crawler/crawler.py.py
+++ crawler/crawler.py.py
@@ -1,34 +1,37 @@
 # Databricks notebook source
-
-#stocks
-url="https://merolagani.com/BrokerList.aspx"
-
-# COMMAND ----------
-
 import requests
 from bs4 import BeautifulSoup
 
+def f_crawl_item(url:str)->list:
+
+    """
+    function takes url input and returns
+    lists of crawled items
+    """
+    page = requests.get(url)
+    html = BeautifulSoup(page.text, "html.parser")
+    quotes = html.find('table')
+    items=[]
+    for index, tr in enumerate(html.findAll('table')[0].findAll('tr')):
+        if index != 0:
+            broker_id_tag = tr.td.a
+            broker_name_tag = broker_id_tag.find_parent().findNextSibling()
+            broker_phone_tag = broker_name_tag.findNextSibling()
+            broker_address_tag=broker_phone.findNextSibling()
+            broker_tuple=(broker_id_tag.text, broker_name_tag.text, broker_phone_tag.text, broker_address_tag.text)
+            items.append(broker_tuple)
+    return items
+
+
+
+url="https://merolagani.com/BrokerList.aspx"
+print(f_crawl_item(url))
+
 # COMMAND ----------
 
-page = requests.get(url)
+
+a=('name', )
 
 # COMMAND ----------
 
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find('div', id="ctl00_ContentPlaceHolder1_divData")
-items=[]
-
-for row in results.findAll('table'):
-    # item={}
-    # item['id']=row.td.text
-    # items.append(item)
-
-print(item)
-
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
 
